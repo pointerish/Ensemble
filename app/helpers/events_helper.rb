0c6bc49c42d6da9e1b03236c297d909259f5676a
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 15:24:00 -0600

    Add helper methods for Session and Event.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def attending?(event_id, user_id)
+    found = nil
+    Event.find(event_id).attendants.each do |attendant|
+      found = attendant.user_id == user_id
+    end
+    found
+  end
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,31 @@
+module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+end
