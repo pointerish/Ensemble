55ab2b614b790c61213ee146fd75822f430827e6
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 15:18:00 -0600

    Add Attendant controller and methods for ApplicationController.

--- app/controllers/attendants_controller.rb
+++ app/controllers/attendants_controller.rb
@@ -1,2 +1,16 @@
-class AttendantsController < ApplicationController
+class AttendantController < ApplicationController
+  def new
+    @attendant = Attendant.new
+  end
+
+  def create
+    @attendant = Attendant.new(attendant_args)
+    redirect_to root_path if @attendant.save
+  end
+
+  private
+
+  def attendant_args
+    params.permit(:user_id, :event_id)
+  end
 end
