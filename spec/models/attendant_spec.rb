11d8158115b7c94d600b01bdab621d85354cfd6a
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 17:30:42 -0600

    Complete tests for Event model.

--- spec/models/attendant_spec.rb
+++ spec/models/attendant_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Attendant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without an event_id"
+  it "is not valid without an user_id"
 end
