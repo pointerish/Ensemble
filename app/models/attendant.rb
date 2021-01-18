daf52b60b8699a37024df050c69ed565d6f3d569
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 15:26:02 -0600

    Add Attendant and Event model.

--- app/models/attendant.rb
+++ app/models/attendant.rb
@@ -1,2 +1,6 @@
 class Attendant < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+
+  scope :my_events, ->(user_identifier) { where('user_id = ?', user_identifier) }
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,21 @@
+# rubocop:disable Naming/AccessorMethodName
+
 class Event < ApplicationRecord
+  include SessionsHelper
+
+  has_many :attendants
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :location, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :date, presence: true
+  validates :created_by, presence: true
+
+  scope :upcoming, -> { where('date >= ?', Time.zone.now) }
+  scope :previous, -> { where('date < ?', Time.zone.now) }
+
+  def set_user!(user)
+    self.created_by = user.id
+    save!
+  end
 end
+
+# rubocop:enable Naming/AccessorMethodName
