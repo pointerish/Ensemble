f82b624b5860df822259b0b6d72785c22b53306f
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 15:48:33 -0600

    Fix partial file name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :attendants
-  resources :users
+  root :to => "events#index"
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/attend', to: 'attendant#create'
   resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
