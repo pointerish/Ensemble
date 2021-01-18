15559dccf224bf165458998655f8f6453d972676
Josias Alvarado <josiasjag@gmail.com>
2021-01-18 15:28:26 -0600

    Run migrations and seeder script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([{ username: 'pointerish' }, { username: 'anerlo' }, { username: 'adrianux2' }, { username: 'salsita' }])
+Event.create(name: 'Pointerish Event', location: 'Pointerish\'s Discord', created_by: 1, date:'2021-01-16 13:00:00')
+Event.create(name: 'Anerlo Event', location: 'Anerlo\'s Discord', created_by: 2, date:'2021-01-16 14:00:00')
+Event.create(name: 'Adrianux2 Event', location: 'Adrianux2\'s Discord', created_by: 3, date:'2021-01-16 19:00:00')
+Event.create(name: 'Salsita Event', location: 'Salsita\'s Discord', created_by: 4, date:'2021-01-16 23:00:00')
